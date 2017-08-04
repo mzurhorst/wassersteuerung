a2228465ff998790f35c7876999dd0c396b4b320
Marcus Zurhorst <marcuszurhorst@gmail.com>
2017-08-04 18:20:16 +0200

    white space fixes

--- settings.py
+++ settings.py
@@ -19,39 +19,39 @@ debug = True      # debug messages enabled
 
 
 def get_forecast_settings():
-	""" Gets the settings for the forecast
+    """ Gets the settings for the forecast
 
-	This public function reads the settings for the forecast from the OpenWeatherMap.org data.
-	It considers the forecast days and the forecast progression factor.
+    This public function reads the settings for the forecast from the OpenWeatherMap.org data.
+    It considers the forecast days and the forecast progression factor.
 
-	Returns:
-	    []:  list of forecast_days (int), forecast_progression (float)
-	"""
+    Returns:
+        []:  list of forecast_days (int), forecast_progression (float)
+    """
 
-	import configparser
-	
-	list = []
-	
-	config=configparser.ConfigParser()
-	config.read("settings.ini")
-	
-	
-	try:
-		forecast_days = int(config.get("General", "forecast_days"))
-	except ValueError:
-		# 2 days is a reasonable default setting.
-		forecast_days = 2
-	list.append(forecast_days)
+    import configparser
+
+    list = []
+
+    config=configparser.ConfigParser()
+    config.read("settings.ini")
 
 
-	try:
-		forecast_progression = config.get("General", "forecast_progression")
-	except ValueError:
-		# 0.85 is a reasonable default setting.
-		foreacast_progression = 0.85
-	list.append(forecast_progression)
-	
-	return list
+    try:
+        forecast_days = int(config.get("General", "forecast_days"))
+    except ValueError:
+        # 2 days is a reasonable default setting.
+        forecast_days = 2
+    list.append(forecast_days)
+
+
+    try:
+        forecast_progression = config.get("General", "forecast_progression")
+    except ValueError:
+        # 0.85 is a reasonable default setting.
+        foreacast_progression = 0.85
+    list.append(forecast_progression)
+
+    return list
 
 
 get_forecast_settings()
@@ -62,9 +62,9 @@ get_forecast_settings()
 #print("Beschreibung für Ventil 1 (vor Update): ", config.get("Valve1", "description"))
 
 #if config.getboolean("Valve2", "isactive"):
-	#print("Status für Ventil 2:  Aktiviert")
+    #print("Status für Ventil 2:  Aktiviert")
 #else:
-	#print("Status für Ventil 2:  Nicht aktiviert")
+    #print("Status für Ventil 2:  Nicht aktiviert")
 
 
 #togglevar = config.get("Valve1", "description")
