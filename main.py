c89a6592cd00b1c9242564c15a1e88ffe079cd7c
Marcus Zurhorst <marcuszurhorst@gmail.com>
2018-02-04 14:21:00 +0100

    Will use web-based frontend instead of Kivy.

--- main.py
+++ main.py
@@ -1,26 +1,61 @@
-from kivy.config import Config
-Config.set('graphics',  'resizeable',  False)
-Config.set('graphics',  'width',  '800')
-Config.set('graphics',  'height',  '480')
+from flask import Flask, render_template
+app = Flask(__name__)
 
-# from kivy.core.window import Window
-# Window.borderless = True
 
-from kivy.app import App
+@app.route('/')
+def hello():
+    return "Hello World - Test 2"
 
-class GartenbewaesserungApp(App):
-    def  testfunktion(self,  *args):
-       print ("Testausgabe") 
-       if args[1]:
-           self.Status1 = "Device On"
-           print("Turning On")
-       else:
-           self.Status1 = "Device Off"
-           print("Turning Off")       
-       # exit(0)
+@app.route('/heyhoo/')
+def hello2():
+    return render_template('heyhoo.html')
+
+
 
 
 if __name__ == '__main__':
-    
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#from kivy.config import Config
+#Config.set('graphics',  'resizeable',  False)
+#Config.set('graphics',  'width',  '800')
+#Config.set('graphics',  'height',  '480')
+
+## from kivy.core.window import Window
+## Window.borderless = True
+
+#from kivy.properties import ObjectProperty
+#from kivy.app import App
+
+#class GartenbewaesserungApp(App):
+    #def  testfunktion(self,  *args):
+       #print ("Testausgabe") 
+       #print("Args[1] Typ = ", type(args[1]))
+       #print(args[1])
+       #if args[1] == "down":
+           #print("Turning On")
+           ##self.root.ids.popuptextinput.disabled = True  
+       #else:
+           #print("Turning Off")       
+           ##self.ids.popuptextinput.disabled = False  
+       ## exit(0)
+       
+       
+
+
+if __name__ == '__main__':
     GartenbewaesserungApp().run()
 
