1cea7e85b1d0633e571124893f1ed0f2393b4d1f
Marcus Zurhorst <marcuszurhorst@gmail.com>
2018-02-04 16:27:51 +0100

    Flask trials

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, abort, request, render_template
 app = Flask(__name__)
 app.static_folder = 'static'
 
+@app.before_request
+def limit_remote_addr():
+    if request.remote_addr != '127.0.0.1':
+        abort(403)  # Forbidden
 
 @app.route('/')
 def hello():
-    return "Hello World - TesghfghIch habet 2"
+    return render_template('index.html')
 
 @app.route('/heyhoo/')
 def hello2():
@@ -16,3 +20,8 @@ def hello2():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+    
+
+
